aa6ed95dfb7b73288a18787a6bf8c790914407ff
teyfikgokdemir <teyfikgokdemir@outlook.com>
2026-07-25 16:28:57 +0300

    Fix sitemap deployment without external integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://teyfikgokdemir.com',
   trailingSlash: 'always',
-  integrations: [sitemap()],
 });
