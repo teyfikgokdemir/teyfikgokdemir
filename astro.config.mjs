9c1ea8f2b94133c10d944823f14d12f2ae81161e
teyfikgokdemir <teyfikgokdemir@outlook.com>
2026-07-25 16:22:20 +0300

    Enable automatic sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://teyfikgokdemir.com',
   trailingSlash: 'always',
+  integrations: [sitemap()],
 });
