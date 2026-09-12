9945290e17a69e40667d8269898be5a1e57131b9
teyfikgokdemir <teyfikgokdemir@outlook.com>
2026-09-12 18:00:43 +0300

    Load mobile responsive overrides

--- growth-os/apps/web/src/app/layout.tsx
+++ growth-os/apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import './styles.css';
 import './modules.css';
 import './modules-v2.css';
 import './report.css';
+import './mobile-overrides.css';
 import NavigationPersistence from './components/NavigationPersistence';
 import OverviewIntelligenceHost from './components/OverviewIntelligenceHost';
 import AgencyPortfolioHost from './components/AgencyPortfolioHost';
